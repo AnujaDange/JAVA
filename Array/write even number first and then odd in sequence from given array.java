2f953b91b21fd227f89c96b1b1a288ee18fda4e2
AnujaDange <92947707+AnujaDange@users.noreply.github.com>
2022-09-21 12:17:04 +0530

    Add files via upload

---
+++ Array/write even number first and then odd in sequence from given array.java
@@ -0,0 +1,22 @@
+package array_display;
+
+public class array_arranging {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a[]={2,3,7,9,28,79};
+		for(int i=0;i<a.length;i++){  
+			if(a[i]%2==0){  
+			System.out.println(a[i]);  
+			}  
+			}  
+		for(int i=0;i<a.length;i++){  
+		if(a[i]%2!=0){  
+		System.out.println(a[i]);  
+		}  
+		}  
+		
+	}
+
+}
+	
