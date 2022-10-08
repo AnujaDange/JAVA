2a6e1db7dd036ee4275123202e9de9737fb4e2b5
AnujaDange <92947707+AnujaDange@users.noreply.github.com>
2022-10-08 21:59:18 +0530

    Add files via upload

---
+++ All programs/program to print factorial of given number.java
@@ -0,0 +1,18 @@
+package factorial_number;
+import java.util.Scanner;
+public class Fact_number {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter any number => ");
+		int no = sc.nextInt();
+		int fact=1;
+		for(int i=1 ;i<=no ;i++)
+		{
+			fact=fact*i;
+		}
+		System.out.println(fact);
+	}
+
+}
