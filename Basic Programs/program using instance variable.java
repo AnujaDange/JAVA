1e37457a8dcd78e0a3834d11ab3a08f5924ca559
AnujaDange <92947707+AnujaDange@users.noreply.github.com>
2022-12-01 22:45:54 +0530

    Add files via upload

---
+++ Basic Programs/program using instance variable.java
@@ -0,0 +1,12 @@
+public class Student
+	{
+      //instance variable
+       int age;
+	  public static void main(String [] args)
+	    {
+ 		   Student obj =new Student();
+		   System.out.println("Student age="+obj.age);
+
+	    } 
+	  
+	}
