f1dea833bfa6bb14f5973e8419dd58371a5141e7
AnujaDange <92947707+AnujaDange@users.noreply.github.com>
2022-12-20 23:03:55 +0530

    Add files via upload

---
+++ Number Conversions/Write a program to convert DECIMAL to BINARY.java
@@ -0,0 +1,19 @@
+import	java.util.*;	
+public class Dectobin		
+{	
+public static void main(String[]	args)	
+{	
+	 System.out.println("Enter the decimal number");	
+	 Scanner sc=new	Scanner(System.in);	
+	 int n=sc.nextInt();	
+	 String	bin="";	
+	 while(n>0)	
+	 {	
+	     int r=n%2;	
+	     bin=	r+bin;	
+	     n=n/2;	
+			
+	 }	
+	 System.out.println("Binary Equivalent:"+bin);	
+}	
+}
