6078bc05f8a85c424f65e8d2e068cd9f8899ce82
AnujaDange <92947707+AnujaDange@users.noreply.github.com>
2023-01-27 21:40:12 +0530

    Add files via upload

---
+++ Operators/UnaryOperator.java
@@ -0,0 +1,18 @@
+public class UnaryOperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      int a=5;
+      System.out.println("Postfix increment=");
+      System.out.println(a++);
+      System.out.println("Postfix decrement=");
+      System.out.println(a--);
+      System.out.println("Prefix decrement=");
+      System.out.println(--a);
+      System.out.println("Prefix increment=");
+      System.out.println(++a);
+     
+      
+	}
+
+}
