e39f1c9ea79f4d1b81727f1c49e5adacb79a2a58
AnujaDange <92947707+AnujaDange@users.noreply.github.com>
2023-01-29 23:57:33 +0530

    Add files via upload

---
+++ Operators/RelationalOperator.java
@@ -0,0 +1,33 @@
+class LogicalOperator
+{
+  publis static void main(String[] args)
+  {
+   int a=5;
+   int b=10;
+   if(a==b)
+      System.out.println("a and b are equal");
+else
+      System.out.println("a and b are not equal");
+   if(a>b)
+      System.out.println("a greater than b");
+else
+      System.out.println("b greater than a");
+  if(a<b)
+      System.out.println("a less than b");
+else
+      System.out.println("b less than a");
+  if(a!=b)
+      System.out.println("a is not equal to b");
+else
+      System.out.println("a is equal to b");
+   if(a>=b)
+      System.out.println("a greater than or equal to b");
+else
+      System.out.println("a not greater than or equal to a");
+  if(a<=b)
+      System.out.println("a less than or equal to b");
+else
+      System.out.println("a not less than or equal to b");
+}
+
+}
